06dc3f1effda9da2d8a0f3ba779acdd6178d15c4
huma17ax <genbot.think17Virt@gmail.com>
2025-06-13 23:49:39 +0900

    [feat] PWA機能を追加し、アプリのマニフェストを設定

--- app.vue
+++ app.vue
@@ -1,4 +1,5 @@
 <template>
+  <NuxtPwaManifest />
   <ion-app>
     <NuxtPage />
   </ion-app>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,55 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   srcDir: 'src/',
+  app: {
+    head: {
+      link: [
+        { rel: 'manifest', href: '/manifest.webmanifest' }
+      ]
+    }
+  },
+  modules: [
+    '@vite-pwa/nuxt'
+  ],
+  pwa: {
+    registerType: 'autoUpdate',
+    workbox: {
+      navigateFallback: '/',
+      navigateFallbackDenylist: [/^\/__\/auth\/.*/],
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+    },
+    manifest: {
+      name: 'Trip Tail',
+      short_name: 'TripTail',
+      description: '旅行の記録を管理するアプリ',
+      theme_color: '#7aa3da',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      scope: '/',
+      icons: [
+        {
+          src: '/favicon.ico',
+          sizes: '48x48',
+          type: 'image/x-icon'
+        },
+        {
+          src: '/favicon.ico',
+          sizes: '192x192',
+          type: 'image/x-icon'
+        },
+        {
+          src: '/favicon.ico',
+          sizes: '512x512',
+          type: 'image/x-icon'
+        }
+      ]
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module'
+    }
+  },
   nitro: {
     output: {
         dir: 'dist/',
